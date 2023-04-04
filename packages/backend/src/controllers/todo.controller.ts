4abc2c9c7d32490d28507419f8aa1d0261ad586f
crackfield <crackfield777@gmail.com>
2023-04-04 12:23:20 +0300

    feat: Create controller

--- packages/backend/src/controllers/todo.controller.ts
+++ packages/backend/src/controllers/todo.controller.ts
@@ -1,13 +1,53 @@
 import { Response, Request } from 'express';
+import { ITodo } from '../types/todos.type';
 import TodoService from '../services/todo.service';
 
 export class TodoController {
   constructor(private todoService: TodoService) {}
 
-  async getAllTodo(_: Request, res: Response) {
-    // TODO: Write your implementation here
+  async getAllTodos(_: Request, res: Response) {
     const todos = await this.todoService.findAll();
     res.send(todos);
+  }
+
+  async createTodo(req: Request, res: Response) {
+    // TODO: validation
+    const { name, description, isCompleted, isPrivate }: ITodo = req.body;
+    const todo = await this.todoService.create({ name, description, isCompleted, isPrivate });
+    res.send(todo);
+  }
+
+  async updateTodo(req: Request, res: Response) {
+    // TODO: validation
+    const { todoId } = req.params;
+    const { name, description, isCompleted, isPrivate }: ITodo = req.body;
+    const todo = await this.todoService.update(todoId, {
+      name,
+      description,
+      isCompleted,
+      isPrivate
+    });
+    res.send(todo);
+  }
+
+  async togglePrivate(req: Request, res: Response) {
+    // TODO: validation
+    const { todoId } = req.params;
+    const todo = await this.todoService.togglePrivate(todoId);
+    res.send(todo);
+  }
+
+  async toggleCompleted(req: Request, res: Response) {
+    // TODO: validation
+    const { todoId } = req.params;
+    const todo = await this.todoService.toggleCompleted(todoId);
+    res.send(todo);
+  }
+
+  async deleteTodo(req: Request, res: Response) {
+    const { id } = req.body;
+    const todo = await this.todoService.delete(id);
+    res.send(todo);
   }
 }
 
