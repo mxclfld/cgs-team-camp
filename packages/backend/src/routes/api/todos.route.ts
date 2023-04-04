9349ddddb7685923ee2f83653599442180939b50
crackfield <crackfield777@gmail.com>
2023-04-04 12:23:54 +0300

    feat: Create routes

--- packages/backend/src/routes/api/todos.route.ts
+++ packages/backend/src/routes/api/todos.route.ts
@@ -4,6 +4,16 @@ import todoController from '../../controllers/todo.controller';
 
 const todosRouter: Router = Router();
 
-todosRouter.get('', todoController.getAllTodo.bind(todoController));
+todosRouter.get('/', todoController.getAllTodos.bind(todoController));
+
+todosRouter.post('/', todoController.createTodo.bind(todoController));
+
+todosRouter.put('/:todoId', todoController.updateTodo.bind(todoController));
+
+todosRouter.patch('/:todoId/private', todoController.toggleCompleted.bind(todoController));
+
+todosRouter.patch('/:todoId/complete', todoController.toggleCompleted.bind(todoController));
+
+todosRouter.delete(':todoId', todoController.deleteTodo.bind(todoController));
 
 export default todosRouter;
