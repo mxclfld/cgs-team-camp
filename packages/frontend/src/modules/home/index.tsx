1a5e43266e0d68c8147992a6dc865602a8525692
crackfield <crackfield777@gmail.com>
2023-04-11 05:31:08 +0300

    feat: Create homepage

--- packages/frontend/src/modules/home/index.tsx
+++ packages/frontend/src/modules/home/index.tsx
@@ -1,5 +1,41 @@
 import React from 'react';
+import { Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { PageWrapper } from '../common/components/wrapper/wrapper.component';
+import { APP_KEYS } from '../common/consts';
 
-const HomePageContainer = () => <div>React is ready HOME</div>;
+const HomePageContainer = () => (
+  <PageWrapper>
+    <Typography variant="h3" sx={{ textAlign: 'center' }}>
+      Todo App
+    </Typography>
+    <Button
+      type="button"
+      variant="contained"
+      component={Link}
+      to={APP_KEYS.ROUTER_KEYS.LOGIN}
+      fullWidth
+    >
+      Login
+    </Button>
+    <Button
+      type="button"
+      variant="contained"
+      component={Link}
+      to={APP_KEYS.ROUTER_KEYS.REGISTER}
+      fullWidth
+    >
+      Register
+    </Button>
+    <Button
+      type="button"
+      sx={{ alignSelf: 'center' }}
+      component={Link}
+      to={APP_KEYS.ROUTER_KEYS.ROOT}
+    >
+      Forgot password?
+    </Button>
+  </PageWrapper>
+);
 
 export default HomePageContainer;
