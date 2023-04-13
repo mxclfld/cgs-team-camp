bd98f1a5e70362e3738dd9e28fad54bcc487f006
crackfield <crackfield777@gmail.com>
2023-04-13 17:29:12 +0300

    feat: Create page for managing profile and update navigation file

--- packages/frontend/src/modules/navigation/index.tsx
+++ packages/frontend/src/modules/navigation/index.tsx
@@ -1,38 +1,24 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePageContainer from '../home';
 import { APP_KEYS } from '../common/consts';
 import { Login } from '../auth/login/login.component';
 import { Register } from '../auth/register/register.component';
 import { TodoContainer } from '../todo/containers/todoContainer/todo-container.component';
-import { Profile } from '../auth/profile/profile.component';
+import { ChangePassword } from '../auth/changePassword/change-password.component';
 import { ViewTodo } from '../todo/components/viewTodo/view-todo.component';
+import ProfilePageContainer from '../profile';
 
-const router = createBrowserRouter([
-  {
-    path: APP_KEYS.ROUTER_KEYS.ROOT,
-    element: <HomePageContainer />
-  },
-  {
-    path: APP_KEYS.ROUTER_KEYS.LOGIN,
-    element: <Login />
-  },
-  {
-    path: APP_KEYS.ROUTER_KEYS.REGISTER,
-    element: <Register />
-  },
-  {
-    path: APP_KEYS.ROUTER_KEYS.TODOS_LIST,
-    element: <TodoContainer />
-  },
-  {
-    path: APP_KEYS.ROUTER_KEYS.TODO_ITEM,
-    element: <ViewTodo />
-  },
-  {
-    path: APP_KEYS.ROUTER_KEYS.PROFILE,
-    element: <Profile />
-  }
-]);
-
-export const MainRouter = () => <RouterProvider router={router} />;
+export const MainRouter = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path={APP_KEYS.ROUTER_KEYS.ROOT} element={<HomePageContainer />} />
+      <Route path={APP_KEYS.ROUTER_KEYS.LOGIN} element={<Login />} />
+      <Route path={APP_KEYS.ROUTER_KEYS.REGISTER} element={<Register />} />
+      <Route path={APP_KEYS.ROUTER_KEYS.TODOS_LIST} element={<TodoContainer />} />
+      <Route path={APP_KEYS.ROUTER_KEYS.TODO_ITEM} element={<ViewTodo />} />
+      <Route path={APP_KEYS.ROUTER_KEYS.PROFILE} element={<ProfilePageContainer />} />
+      <Route path={APP_KEYS.ROUTER_KEYS.CHANGE_PASSWORD} element={<ChangePassword />} />
+    </Routes>
+  </BrowserRouter>
+);
