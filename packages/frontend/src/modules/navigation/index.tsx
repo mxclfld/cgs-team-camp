ea72e92c13ae12e4460f444c0de3470a3e4ed85b
crackfield <crackfield777@gmail.com>
2023-04-17 16:12:44 +0300

    feat: secure roots

--- packages/frontend/src/modules/navigation/index.tsx
+++ packages/frontend/src/modules/navigation/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import HomePageContainer from '../home';
 import { APP_KEYS } from '../common/consts';
 import { Login } from '../auth/login/login.component';
@@ -8,17 +8,36 @@ import { TodoContainer } from '../todo/containers/todoContainer/todo-container.c
 import { ChangePassword } from '../auth/changePassword/change-password.component';
 import { ViewTodo } from '../todo/components/viewTodo/view-todo.component';
 import ProfilePageContainer from '../profile';
+import { authService } from '../auth/services/auth.service';
 
-export const MainRouter = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path={APP_KEYS.ROUTER_KEYS.ROOT} element={<HomePageContainer />} />
-      <Route path={APP_KEYS.ROUTER_KEYS.LOGIN} element={<Login />} />
-      <Route path={APP_KEYS.ROUTER_KEYS.REGISTER} element={<Register />} />
-      <Route path={APP_KEYS.ROUTER_KEYS.TODOS_LIST} element={<TodoContainer />} />
-      <Route path={APP_KEYS.ROUTER_KEYS.TODO_ITEM} element={<ViewTodo />} />
-      <Route path={APP_KEYS.ROUTER_KEYS.PROFILE} element={<ProfilePageContainer />} />
-      <Route path={APP_KEYS.ROUTER_KEYS.CHANGE_PASSWORD} element={<ChangePassword />} />
-    </Routes>
-  </BrowserRouter>
-);
+const NavigateToRoot = () => <Navigate to={APP_KEYS.ROUTER_KEYS.ROOT} />;
+
+export const MainRouter = () => {
+  const isAuth = authService.isLoggedIn();
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={APP_KEYS.ROUTER_KEYS.ROOT} element={<HomePageContainer />} />
+        <Route path={APP_KEYS.ROUTER_KEYS.LOGIN} element={<Login />} />
+        <Route path={APP_KEYS.ROUTER_KEYS.REGISTER} element={<Register />} />
+        <Route
+          path={APP_KEYS.ROUTER_KEYS.TODOS_LIST}
+          element={isAuth ? <TodoContainer /> : <NavigateToRoot />}
+        />
+        <Route
+          path={APP_KEYS.ROUTER_KEYS.TODO_ITEM}
+          element={isAuth ? <ViewTodo /> : <NavigateToRoot />}
+        />
+        <Route
+          path={APP_KEYS.ROUTER_KEYS.PROFILE}
+          element={isAuth ? <ProfilePageContainer /> : <NavigateToRoot />}
+        />
+        <Route
+          path={APP_KEYS.ROUTER_KEYS.CHANGE_PASSWORD}
+          element={isAuth ? <ChangePassword /> : <NavigateToRoot />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
